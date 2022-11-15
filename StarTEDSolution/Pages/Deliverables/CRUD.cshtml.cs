8eaf3be6cd958e1b417c9905647382bdd2166b55
TravisS <113370264+ComradCasper@users.noreply.github.com>
2022-11-15 12:45:44 -0700

    Add project files.

---
+++ StarTEDSolution/Pages/Deliverables/CRUD.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarTEDSolution.Pages.Deliverables
+{
+    public class CRUDModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
